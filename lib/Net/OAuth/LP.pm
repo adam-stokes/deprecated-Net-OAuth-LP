a6886958f0813cbfa36dd16e5284d5c0015fe2fb
Adam Stokes <adam.stokes@ubuntu.com>
2013-07-11 01:03:39 -0400

    Bump ver
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/Net/OAuth/LP.pm
+++ lib/Net/OAuth/LP.pm
@@ -10,7 +10,7 @@ use LWP::UserAgent;
 use Net::OAuth;
 $Net::OAuth::PROTOCOL_VERSION = Net::OAuth::PROTOCOL_VERSION_1_0;
 
-our $VERSION = '0.015_02';
+our $VERSION = '0.015_03';
 
 has consumer_key => (
     is      => 'rw',
