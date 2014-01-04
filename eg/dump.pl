352204fa49d57b5b9cf2e538b1053b012d49304b
Adam Stokes <adam.stokes@ubuntu.com>
2014-01-03 21:47:15 -0500

    call list reference
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- eg/dump.pl
+++ eg/dump.pl
@@ -8,6 +8,7 @@ use 5.14.0;
 use FindBin;
 use lib "$FindBin::Bin/../../lib";
 use Net::OAuth::LP::Client;
+use List::AllUtils qw(first);
 
 use DDP;
 
@@ -16,9 +17,14 @@ $c->staging(1);
 
 my $bug = $c->namespace('Bug')->by_id(859600);
 
-p $bug;
+# p $bug->tasks;
+
+my $bugtask =
+  first { $_->{bug_target_name} =~ /ubuntu-advantage|(Ubuntu)/ } @{$bug->tasks};
+
+p $bugtask;
 
 my $person = $c->namespace('Person')->by_name('~adam-stokes');
 
-p $person;
+# p $person;
 
